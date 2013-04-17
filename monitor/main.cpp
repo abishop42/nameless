2da9b2942c225bedb73e4261a4275661f527c16a
abishop42 <abishop195@gmail.com>
2013-04-17 10:41:15 +1000

    updating makefile and main cpp

--- monitor/main.cpp
+++ monitor/main.cpp
@@ -1,17 +1,39 @@
-#include "file_reader.h"
-#include "runner.h"
-#include "libs/zmq.hpp"
+#include "comms.h"
 
-
-int main()
+void display_args(int argc, char* argv[])
 {
-	runner r = runner();
-	display_run_result(r.run_command("ls -la"));
+	for (int i = 0; i < argc; i++)
+	{
+		std::cout << "arg " << i << " = " << argv[i] << std::endl;
+	}
+}
 
-	file_reader fr = file_reader("/proc/stat");
+int main(int argc, char* argv[])
+{
+	
+	display_args(argc, argv);
 
-	fr.read_file();
-	fr.display();
+	std::string client_address = "tcp://localhost:5555";
+	std::string server_address = "tcp://*:5555";
+
+
+
+	if (strcmp(argv[1], "server") == 0)
+	{
+		std::cout << "server will be built" << std::endl;
+		server_module server = server_module(server_address);
+		server.setup();
+	}
+	else if (strcmp(argv[1],"client") == 0)
+	{
+		std::cout << "client will be built" << std::endl;
+		client_module client = client_module(client_address);
+		client.setup();
+	}
+	else
+	{
+		std::cout << "not going to do anything due to args passed in" << std::endl;
+	}
 
 	return 0;
 }

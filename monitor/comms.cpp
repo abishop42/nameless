27b45d04ea30a3dcd836a8395d461f29968a6771
abishop42 <abishop195@gmail.com>
2013-04-18 04:54:20 +1000

    adding send and receive sample to client_comms

--- monitor/comms.cpp
+++ monitor/comms.cpp
@@ -19,12 +19,22 @@ bool client_module::setup()
 
 void client_module::send_message(std::string message)
 {
-
+	zmq::message_t response (4);
+	memcpy ((void *) response.data (), "done", 4);
+	socket->send (response);
 }
 
 void client_module::receive_message()
 {
+	while (true)
+	{
+		zmq::message_t request;
+		socket->recv(&request);
+		std::string message = std::string(static_cast<char*>(request.data()), request.size());
+		std::cout << "received message: " << message << std::endl;
 
+		this->send_message("I did get stuff");
+	}
 }
 
 server_module::server_module(std::string address) : comms_module(address, 1, ZMQ_REQ)
